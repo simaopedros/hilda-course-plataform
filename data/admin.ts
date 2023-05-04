5cb56599ff9f4ce7bafd93a1eaee23d5e40452ae
Simao Pedro da Silva <simaopedros@gmail.com>
2023-05-04 15:14:33 -0300

    continuação de testes

--- data/admin.ts
+++ data/admin.ts
@@ -1,15 +1,14 @@
 // data/admin.ts
 import * as admin from "firebase-admin";
-const firebasePrivateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY;
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: JSON.parse(`"${firebasePrivateKey}"`),
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
     }),
-    databaseURL: (process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string).replace(/\\n/g, '\n'),
+    databaseURL: (process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string),
     
     
   });
