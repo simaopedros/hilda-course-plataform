79248b2143c7b0788c0522d05e9258630d7401fb
Simao Pedro da Silva <simaopedros@gmail.com>
2023-05-04 07:59:47 -0300

    Correção de variaveis de ambiente

--- data/sdk.ts
+++ data/sdk.ts
@@ -10,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCb4UPMsG5PVLNhlTh1FghRLC6-OTBFOF8",
-  authDomain: "course-plataform.firebaseapp.com",
-  projectId: "course-plataform",
-  storageBucket: "course-plataform.appspot.com",
-  messagingSenderId: "692254745951",
-  appId: "1:692254745951:web:18c6776a159b827ecf42ea",
-  measurementId: "G-HM0QDF66HS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
