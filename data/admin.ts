eebce7fc858d3d3c3d78ed8d0400255fe5e9edd3
Simao Pedro da Silva <simaopedros@gmail.com>
2023-05-04 14:49:11 -0300

    teste webhook

--- data/admin.ts
+++ data/admin.ts
@@ -1,12 +1,13 @@
 // data/admin.ts
 import * as admin from "firebase-admin";
+const firebasePrivateKey = process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY;
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: (process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+      privateKey: JSON.parse(`"${firebasePrivateKey}"`),
     }),
     databaseURL: (process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string).replace(/\\n/g, '\n'),
     
